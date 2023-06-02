598b1074e68128c770353dcbba5b07bc91c632db
Yong Kim <yhkim4573@gmail.com>
2023-06-01 21:30:17 -0400

    Photo component

--- components/Media/Photo.jsx
+++ components/Media/Photo.jsx
@@ -4,7 +4,7 @@ const Photo = () => {
   return (
     <div className="max-h-96 flex border-gray-400 rounded">
       <img
-        src="https://picsum.photos/1920/108"
+        src="https://picsum.photos/1920/1080"
         alt="profile"
         className="object-cover rounded select-none"
       />
